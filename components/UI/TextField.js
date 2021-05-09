65a4f9862a8061817589b9c14ea65f6d9d95ecda
Apostolos Tselios <apostolis.tselios@gmail.com>
2021-05-09 19:18:26 +0300

    Add the Login Page
    Add the Login Component.
    Add the TextField Component in the UI directory.
    Add utils.module.css with utility classes

---
+++ components/UI/TextField.js
@@ -0,0 +1,7 @@
+import { TextField as MuiTextField } from '@material-ui/core';
+
+const TextField = (props) => {
+  return <MuiTextField variant="outlined" {...props} />;
+};
+
+export default TextField;
