0618f94ca00c6355c7598467a1412ac385bf4341
Apostolos Tselios <apostolis.tselios@gmail.com>
2021-05-16 14:27:28 +0300

    Complete the setup for NextJS and Material UI by adding _app.js and _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,40 @@
-import Head from 'next/head';
 import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
 import '../styles/globals.scss';
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = (props) => {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <React.Fragment>
       <Head>
         <title>Codex</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </React.Fragment>
   );
+};
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
 };
 
 export default MyApp;
