00e6bab89217e33f82ab0155f39ea5d45b64cbc2
Apostolos Tselios <apostolis.tselios@gmail.com>
2021-06-29 20:53:03 +0300

    Finalize the grid home page layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,14 @@ const useStyles = makeStyles((theme) => {
   return {
     root: {
       display: 'flex',
+      height: '100%',
     },
     card: {
+      height: '100%',
       padding: '5px',
       marginLeft: '5px',
       marginRight: '5px',
+      backgroundColor: theme.palette.grey[300],
     },
   };
 });
@@ -22,25 +25,23 @@ const Home = () => {
   return (
     <Page>
       <div className={classes.root}>
-        <Grid container direction="column" spacing={1}>
-          <Grid item>
+        <Grid container spacing={1}>
+          <Grid item md={6} xs={12}>
             <Paper className={classes.card} variant="outlined">
               Announcenments
             </Paper>
           </Grid>
-          <Grid item>
+          <Grid item md={6} xs={12}>
             <Paper className={classes.card} variant="outlined">
               Statistics
             </Paper>
           </Grid>
-        </Grid>
-        <Grid container direction="column" spacing={1}>
-          <Grid item>
+          <Grid item md={6} xs={12}>
             <Paper className={classes.card} variant="outlined">
               Profile
             </Paper>
           </Grid>
-          <Grid item>
+          <Grid item md={6} xs={12}>
             <Paper className={classes.card} variant="outlined">
               Notifications
             </Paper>
